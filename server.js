3fc72eeb337030ffdb48c9e1741e84cf0a7656a7
Paul Mullins <paulmullins@protonmail.com>
2018-04-07 04:00:47 -0400

    Imported a nodejs application

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+//const ip = require("ip");// ip.address() 
+
+app.use('/abi', express.static('truffleWorkspace/build/contracts'));
+app.get('/', function (req, res) {
+    fs.readFile('./src/index.html', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
+});
+//Host the app
+app.listen(3042, () => console.log('Example app listening on port 3042!'));
