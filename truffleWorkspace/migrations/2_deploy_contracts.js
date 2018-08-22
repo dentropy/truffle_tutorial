e1a6650fd8e26b40ebeb0a532e8b69cc489022ec
Paul Mullins <paulmullins@protonmail.com>
2018-08-22 18:02:19 -0400

    Updated migrations file to not try and migrate the smart contracts that were deleted

--- truffleWorkspace/migrations/2_deploy_contracts.js
+++ truffleWorkspace/migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-var set_string = artifacts.require("./set_string.sol");
-
 var set_int = artifacts.require("./set_int.sol");
-
+var set_string = artifacts.require("./set_string.sol");
 var permissions_set_string = artifacts.require("./permission_set_string.sol");
 var permissions_set_int = artifacts.require("./permission_set_int.sol");
 var dynamic_set_string = artifacts.require("./dynamic_set_string.sol");
@@ -11,10 +7,8 @@ var dynamic_set_int = artifacts.require("./dynamic_set_int.sol");
 var smart_contract_wallet = artifacts.require("./smart_contract_wallet.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(set_string, "First Example");
   deployer.deploy(set_int);
+  deployer.deploy(set_string, "First Example");
   deployer.deploy(permissions_set_string);
   deployer.deploy(permissions_set_int);
   deployer.deploy(dynamic_set_string, "First Dynamic String");
