fd203c5184c7cd8a78af1978e7970e2d4801eb2f
Paul Mullins <paulmullins@protonmail.com>
2018-08-26 13:15:55 -0400

    Updated permission_set_int.sol to return some code

--- truffleWorkspace/contracts/permission_set_int.sol
+++ truffleWorkspace/contracts/permission_set_int.sol
@@ -7,15 +7,20 @@ contract permission_set_int {
         if(owner == address(0)){
             owner = msg.sender;
         }
+        public_int = 420;
     }
-    function set_int_data(int256 tmpInt) public {
+    function set_int_data(int256 tmpInt) public returns(bool) {
         if(msg.sender == owner){
             public_int = tmpInt;
+            return true;
         }
+        return false;
     }
-    function change_owner(address _new_owner) public {
+    function change_owner(address _new_owner) public returns(bool) {
         if(msg.sender == owner){
             owner = _new_owner;
+            return true;
         }
+        return false;
     }    
 }
