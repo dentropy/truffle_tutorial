b6bea7f67de3c45b1afb25d53d590f44b0669ec8
Paul Mullins <paulmullins@protonmail.com>
2018-08-27 11:36:28 -0400

    Fixed bug in migrations

--- truffleWorkspace/migrations/2_deploy_contracts.js
+++ truffleWorkspace/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var set_int = artifacts.require("./set_int.sol");
 var set_string = artifacts.require("./set_string.sol");
-var permissions_set_string = artifacts.require("./permission_set_string.sol");
+var dynamic_set_string = artifacts.require("./dynamic_set_string.sol");
 var permissions_set_int = artifacts.require("./permission_set_int.sol");
 var smart_contract_wallet = artifacts.require("./smart_contract_wallet.sol");
 
