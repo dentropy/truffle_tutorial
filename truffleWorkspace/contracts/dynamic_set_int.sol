da8a73530d24f3af876bef873fb9da18b71caee7
July312018Ubuntu18.04 <paulmullins@protonmail.com>
2018-08-26 23:11:09 -0400

    Updated the dynamic_set_string smart contract but when trying to come up with a UI for it realized I need to properaly design it

--- truffleWorkspace/contracts/dynamic_set_int.sol
+++ truffleWorkspace/contracts/dynamic_set_int.sol
@@ -2,15 +2,20 @@ pragma solidity ^0.4.24;
 
 contract dynamic_set_int {
     int256 public public_int;
-    address inital_owner;
+    address public sudo_owner;
     mapping (address => int256) public int_data;
     mapping (address => bool) public permissions;
     constructor() public {
-        if(inital_owner == address(0)){
-            inital_owner = msg.sender;
+        if(sudo_owner == address(0)){
+            sudo_owner = msg.sender;
             permissions[msg.sender] = true;
         }
     }
+
+    function add_address(address _address) {
+
+    }
+
     function set_int_data(int256 _data) public {
         if(permissions[msg.sender] == true){
             int_data[msg.sender] = _data;
