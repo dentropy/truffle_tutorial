f8dfa0803f4d7e382ec325e3c4ceb364480272b3
GT60-2PC <paul@paul-GT60-2PC>
2018-09-13 15:43:16 -0400

    Removed unneeded code in the server which also removed unneeded code in the static html files

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-//const ip = require("ip");// ip.address() 
-app.use('/src', express.static('src'));
+app.use('/', express.static('src'));
 app.use('/abi', express.static('truffleWorkspace/build/contracts'));
-app.get('/', function (req, res) {
-    fs.readFile('./src/index.html', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.send(data);
-
-        console.log("\n--> Success, you've Requested a page from server.js !!! ");
-    });
-});
-//Host the app
 app.listen(3042, () => console.log('Example app listening on port 3042!'));
